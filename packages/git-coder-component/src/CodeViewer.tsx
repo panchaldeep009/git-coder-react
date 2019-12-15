86ab7d6f759e563e8adee6a13508f5df82a6b7fe
panchaldeep009 <panchaldeep009@gmail.com>
2019-12-14 23:47:54 -0500

    Mono repo setup

---
+++ packages/git-coder-component/src/CodeViewer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CodeViewer = () => {
+    return (
+        <div>
+            Code Viewer
+        </div>
+    )
+}
---
+++ packages/git-coder-example/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CodeViewer } from 'git-coder-react'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <CodeViewer/>
+    </div>
+  );
+}
+
+export default App;
